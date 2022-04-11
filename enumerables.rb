178cee35c4d4d9ff44e88aa672141653265d1890
karenopolanco <karenpalomino97@gmail.com>
2022-04-11 16:36:03 -0400

    first

--- enumerables.rb
+++ enumerables.rb
@@ -13,8 +13,32 @@ class Array
         self.my_each{|ele| new_arr << ele if prc.call(ele)}
         new_arr
     end
+
+    def my_reject(&prc)
+        new_array=[]
+        self.my_each{|ele|new_array << ele if !proc.call(ele)}
+        return new_array
+    end
+    def my_any?(&prc)
+        self.my_each{|ele| return true if proc.call(ele)}
+        return false
+    end
+    def my_all?(&prc)
+        self.my_each {|ele| return false if !proc.call(ele)}
+        true
+    end
 end
 
 a = [1, 2, 3]
 p a.my_select { |num| num > 1 } # => [2, 3]
 p a.my_select { |num| num == 4 } # => []
+
+a = [1, 2, 3]
+p a.my_reject { |num| num > 1 } # => [1]
+p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+
+a = [1, 2, 3]
+p a.my_any? { |num| num > 1 } # => true
+p a.my_any? { |num| num == 4 } # => false
+p a.my_all? { |num| num > 1 } # => false
+p a.my_all? { |num| num < 4 } # => true
