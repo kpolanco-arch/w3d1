e5afa3e8617962004596299eda343c0ffaf94614
Vivian Chen <vnchen92@gmail.com>
2022-04-11 15:43:53 -0400

    Started enumerables

---
+++ enumerables.rb
@@ -0,0 +1,20 @@
+class Array
+    def my_each(&prc)
+        i = 0
+        while i < self.length
+            prc.call(self[i])
+            i += 1
+        end
+        self
+    end
+
+    def my_select(&prc)
+        new_arr = []
+        self.my_each{|ele| new_arr << ele if prc.call(ele)}
+        new_arr
+    end
+end
+
+a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
