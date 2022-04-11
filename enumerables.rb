25cd608aedbcaedb55006f87ceee861bd194ddf2
Vivian Chen <vnchen92@gmail.com>
2022-04-11 17:08:25 -0400

    Updated enumerables

--- enumerables.rb
+++ enumerables.rb
@@ -19,26 +19,43 @@ class Array
         self.my_each{|ele|new_array << ele if !proc.call(ele)}
         return new_array
     end
+
     def my_any?(&prc)
         self.my_each{|ele| return true if proc.call(ele)}
         return false
     end
+
     def my_all?(&prc)
         self.my_each {|ele| return false if !proc.call(ele)}
         true
     end
+
+    def my_flatten    #[1, [1,2]]
+        single = []
+
+        i = 0
+        while i < self.length
+            if self[i].is_a?(Array)
+                single += self[i].my_flatten
+            else
+                single << self[i]
+            end
+            i += 1
+        end
+        # prc = Proc.new { |subarray|
+        #     if subarray.is_a?(Array)
+        #         single += subarray.my_flatten
+        #     else
+        #         single << subarray
+        #     end
+        # }
+
+        # self.my_each{prc}
+        single
+    end
+
+    
+
 end
 
-a = [1, 2, 3]
-p a.my_select { |num| num > 1 } # => [2, 3]
-p a.my_select { |num| num == 4 } # => []
-
-a = [1, 2, 3]
-p a.my_reject { |num| num > 1 } # => [1]
-p a.my_reject { |num| num == 4 } # => [1, 2, 3]
-
-a = [1, 2, 3]
-p a.my_any? { |num| num > 1 } # => true
-p a.my_any? { |num| num == 4 } # => false
-p a.my_all? { |num| num > 1 } # => false
-p a.my_all? { |num| num < 4 } # => true
+p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
